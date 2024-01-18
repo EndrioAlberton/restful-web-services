3db2cd508ae7c3001093df1a7a54dae5e3655421
EndrioAlberton <endrio.alberton@gmail.com>
2024-01-18 11:57:55 -0300

    create project

---
+++ src/test/java/com/javapb/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javapb.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
