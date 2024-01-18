a1803f436ce89575196b5d9b4733bcc406963d27
EndrioAlberton <endrio.alberton@gmail.com>
2024-01-18 12:45:54 -0300

    added: post method and correct HTTP status code

--- src/main/java/com/javapb/restfulwebservices/helloworld/user/UserDaoService.java
+++ src/main/java/com/javapb/restfulwebservices/helloworld/user/UserDaoService.java
@@ -12,12 +12,20 @@ public class UserDaoService {
 	
 	private static List<User> users = new ArrayList<>();
 	
+	private static int usersCount = 0;
+	
 	static {
 		users.add(new User(1,"Endrio",LocalDate.now().minusYears(20)));
 		users.add(new User(2,"Lucas",LocalDate.now().minusYears(35)));
-		users.add(new User(1,"João",LocalDate.now().minusYears(72)));
+		users.add(new User(3,"João",LocalDate.now().minusYears(72)));
 	}
 
+	public User save(User user) {
+		user.setId(++usersCount);
+		users.add(user);
+		return user;
+	}
+	
 	public List<User>findAll(){
 		return users;
 	}
