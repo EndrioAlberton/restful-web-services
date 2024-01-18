174207aa1a097c2b4432ac6f9fd3a375d465d19e
EndrioAlberton <endrio.alberton@gmail.com>
2024-01-18 12:06:05 -0300

    added: hello world and hellow world bean

---
+++ src/main/java/com/javapb/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,23 @@
+package com.javapb.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+
+	private String message;
+	
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "helloWorldBean [message=" + message + "]";
+	}	
+}
